1cb250792a52d31dede2412c93d4b6f1c18494cc
cristianoliveira <public+github@cristianoliveira.com>
2026-01-31 19:18:31 +0100

    fix(nix): add shfmt to nativeBuildInputs
    Ensure shfmt is available in the Nix build sandbox, allowing make check-fmt to pass.
    This resolves the nix build failure due to missing shfmt executable.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,42 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     utils.url = "github:numtide/flake-utils";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
-  outputs = { nixpkgs, utils, ... }:
+  outputs = { nixpkgs, utils, git-hooks, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            shellcheck = {
+              enable = true;
+              files = "\\.(sh|bats)$";
+              exclude = "^tests/";
+              args = [ "-e" "SC2034" "-e" "SC1091" ];
+            };
+            shfmt = {
+              enable = true;
+              files = "\\.(sh|bats|tmux)$";
+              # auto-fix enabled
+              entry = "${pkgs.shfmt}/bin/shfmt -w -i 4";
+            };
+            bats = {
+              enable = true;
+              files = "^tests/.*\\.bats$";
+              # run bats on modified test files
+              entry = "${pkgs.bats}/bin/bats";
+            };
+          };
+        };
       in {
+        checks = {
+          pre-commit-check = pre-commit-check;
+        };
+
         devShells.default = pkgs.mkShell {
+          inherit (pre-commit-check) shellHook;
           packages = with pkgs; [
             bash
             bats
@@ -18,6 +47,7 @@
 
             pre-commit
           ];
+          buildInputs = pre-commit-check.enabledPackages;
         };
 
         packages = {
@@ -25,7 +55,7 @@
             pname = "tmux-intray";
             version = "0.1.0";
             src = ./.;
-            nativeBuildInputs = with pkgs; [ bats tmux ];
+            nativeBuildInputs = with pkgs; [ bats tmux shfmt ];
             preBuild = ''
               export XDG_STATE_HOME=$(mktemp -d)
               export XDG_CONFIG_HOME=$(mktemp -d)
@@ -40,4 +70,3 @@
         };
     });
 }
-# force rebuild
