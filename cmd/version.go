9edc39e5ee92b30f09995fd3a2474c016a28c5c2
cristianoliveira <public+github@cristianoliveira.com>
2026-02-03 20:57:42 +0100

    chore: remove internal commands

--- cmd/version.go
+++ cmd/version.go
@@ -1,8 +1,13 @@
 // Package version implements the version command.
 package version
 
+import "fmt"
+
+// Version is the version of tmux-intray.
+const Version = "0.1.0"
+
 // Run executes the version command.
 func Run(args []string) error {
-	// TODO: Implement version command.
+	fmt.Printf("tmux-intray v%s\n", Version)
 	return nil
 }
--- internal/commands/list/list.go
+++
@@ -1,8 +0,0 @@
-// Package list implements the list command.
-package list
-
-// Run executes the list command.
-func Run(args []string) error {
-	// TODO: Implement list command.
-	return nil
-}
